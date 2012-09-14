19d2ab3b5dc4a9cba4835499e8d6cc49f1c402e8
Doug Rogers <doug.rogers@mandiant.com>
2012-09-14 00:53:25 -0400

    Add install script; use ~/.dr/site to store site name.

---
+++ os/install-unix.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright (c) 2012 Doug Rogers under the terms of the MIT License.
+# See http://www.opensource.org/licenses/mit-license.html..
+# $Id$
+
+if [ -z "$1" ]; then
+    echo "Usage: install-unix.sh <site-name>"
+    echo
+    echo "Creates ~/.dr/site and installs .bashrc, .emacs, .screenrc, etc. to \$HOME."
+    echo
+    exit 1
+fi
+
+run()
+{
+    cmd="$*"
+    echo "$cmd"
+    eval "$cmd"
+}
+
+
+site="$1"
+save="~/.dr/save"
+
+run mkdir -p "$save"
+run "echo $site > ~/.dr/site"
+
+run mv ~/.bashrc "$save"
+run cp ~/dr/os/bashrc-main.sh ~/.bashrc
+
+run mv ~/.screenrc "$save"
+run cp ~/dr/os/screenrc-main ~/.screenrc
+
+run mv ~/.emacs "$save"
+run cp ~/dr/os/emacs-main.el ~/.emacs
