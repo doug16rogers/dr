cb26adf16316f48f7ce59c9fea1fa4d20ccd879e
Doug Rogers <doug.rogers@mandiant.com>
2012-09-18 10:21:57 -0400

    Break out some of the common bash settings to os/unix.

---
+++ os/Darwin/bashrc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH="$HOME/dr/os/$DR_OS/bin:$PATH"
+
+# Pick up my standard bashrc stuff.
+source "$HOME/dr/os/unix/bashrc.sh"
---
+++ os/unix/bashrc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Bash settings.
+
+export EDITOR=emacs
+export CLICOLOR=1
+export HISTCONTROL=ignoreboth
+
+source "$HOME/dr/os/unix/prompt.sh"
