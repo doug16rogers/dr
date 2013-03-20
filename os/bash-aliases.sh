7810a484fcc3f48727292cd182163591ff437e3f
Doug Rogers <dr@nowhere.com>
2013-03-20 16:33:10 -0400

    Add lua-mode.el; replace gd with mcd; use gd for git diff.

--- os/bash-aliases.sh
+++ os/bash-aliases.sh
@@ -4,6 +4,11 @@
 
 alias grepwd="cat $HOME/dr/fun/itasoftware-puzzles/word.lst | grep "
 
+alias glog="git log --name-status "
+alias gb="git branch -a "
+alias gd="git diff "
+alias gs="git status "
+
 alias svnst="svn status -q "
 
 svnstn()
