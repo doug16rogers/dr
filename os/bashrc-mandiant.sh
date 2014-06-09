0daf8fe7c95a6479a5baf3bed226bdab6e19be3c
Doug Rogers <dr@nowhere.com>
2014-06-09 15:46:57 -0400

    Add function for adding remote tags; put FE-specific stuff in zip file.

--- os/bashrc-mandiant.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Use my Mandiant email for git when I'm here.
-# export GIT_AUTHOR_EMAIL="doug.rogers@mandiant.com"  # git config --global user.email  "doug.rogers@mandiant.com"
-
-export GRADLE_HOME="/Users/drogers/Development/gradle-1.6/bin"
-export PATH="$PATH:$GRADLE_HOME"
