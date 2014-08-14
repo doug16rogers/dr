5866018b71d7f90b2454edfb5565a90f31b94d40
Doug Rogers <dr@nowhere.com>
2014-08-14 15:42:45 -0400

    Restore use of '#' for prompt when logged in as root.

--- os/bash-prompt.sh
+++ os/bash-prompt.sh
@@ -11,8 +11,10 @@ color_dir="$color_CYAN"
 
 prompt_char="$"
 
-# if [ "`whoami`" == "root" ]; then
-#     prompt_char="#"
-# fi
+user="`whoami`"
+
+if [ $? -eq 0 -a "$user" == "root" ]; then
+    prompt_char="#"
+fi
 
 export PS1="\n$color_time\$(date -u +%y%m%d-%H%M%S) $color_user\u$color_reset@$color_host\h$color_reset $color_dir\w$color_reset\n$prompt_char "
