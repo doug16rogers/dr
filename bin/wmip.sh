766eed8112926952318be4fcc71289bee619a794
Nobody Special <noone@nowhere.com>
2017-04-04 18:07:14 -0400

    Add script to print public ip address.

---
+++ bin/wmip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -q -O - \
+     -U 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0' \
+     http://whatismyipaddress.com \
+| grep -i 'click for more about' wimip.out \
+| sed -e 's/^.*lick for more about //' -e 's/".*$//'
