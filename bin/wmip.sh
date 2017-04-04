060af76f124973323cb519a344617f0e110a918a
Nobody Special <noone@nowhere.com>
2017-04-04 18:14:37 -0400

    Remove .out file.

--- bin/wmip.sh
+++ bin/wmip.sh
@@ -3,5 +3,5 @@
 wget -q -O - \
      -U 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0' \
      http://whatismyipaddress.com \
-| grep -i 'click for more about' wimip.out \
+| grep -i 'click for more about' \
 | sed -e 's/^.*lick for more about //' -e 's/".*$//'
