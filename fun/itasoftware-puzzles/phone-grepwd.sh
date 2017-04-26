a3e3ae84c065da061c002500e5dda0704bd29c35
Doug Rogers <dr@nowhere.com>
2017-04-26 15:23:28 -0400

    Add search for up to three words in phone numbers.

--- fun/itasoftware-puzzles/phone-grepwd.sh
+++ fun/itasoftware-puzzles/phone-grepwd.sh
@@ -37,29 +37,113 @@ if [ -z "$number" ]; then
     exit 1
 fi
 
-regex=""
 i=0
+regex_i=0
 while [ $i -lt ${#number} ]; do
     letter=${number:$i:1}
     is_digit=0
+    is_letter=0
     case $letter in
         [0-9])
             is_digit=1
+            ;;
+        [A-Za-z])
+            is_letter=1
             ;;
         *)
     esac
 
     if [ $is_digit -eq 1 ]; then
-        regex="${regex}[${letters[$letter]}]"
-    else
-        regex="${regex}$letter"
+        regex[$regex_i]="[${letters[$letter]}]"
+        regex_i=$[regex_i+1]
+    elif [ $is_letter -eq 1 ]; then
+        regex[$regex_i]="$letter"
+        regex_i=$[regex_i+1]
     fi
     i=$[i + 1]
 done
 
+re=`echo "${regex[@]}" | sed 's/ //g'`
+
 if [ $exact -eq 1 ]; then
-    regex="^$regex\$"
+    re="^$re\$"
 fi
 
-echo "regex=$regex"
-cat "$HOME/dr/fun/itasoftware-puzzles/word.lst" | grep "$regex"
+# Run the full number through the grepper.
+echo "# regex=$re"
+grep "$re" "$HOME/dr/fun/itasoftware-puzzles/word.lst"
+
+n=${#regex[@]}
+
+# Break into pairs of regexes only if at least 4 letters.
+if [ $n -ge 4 ]; then
+    # Now break the full number into all possible pairs of regexes (m=2).
+    m=2
+    for ((i = 1; i < n; i++)) do
+        k=0
+        for ((j = 0; j < m; j++)) do
+            re[$j]=""
+            words[$j]=""
+            grep_result[$j]=0
+        done
+        for ((j = 0; j < n; j++)) do
+            if [ $j -eq $i ]; then
+                k=$[k+1]
+            fi
+            re[$k]="${re[$k]}${regex[$j]}"
+        done
+        # echo "# i=$i re=${re[@]}"
+        all_found=1
+        for ((j = 0; j < m; j++)) do
+            wds=(`grep "^${re[$j]}$" "$HOME/dr/fun/itasoftware-puzzles/word.lst"`)
+            if [ ${#wds[@]} -eq 0 ]; then
+                all_found=0
+                break
+            fi
+            words[$j]="${wds[@]}"
+        done
+        if [ $all_found -eq 1 ]; then
+            # echo "# re=${re[@]}"
+            echo "(${words[0]}) (${words[1]}) "
+        fi
+    done
+fi            
+
+# Break into trios of regexes only if at least 5 letters.
+if [ $n -ge 5 ]; then
+    # Now break the full number into all possible trios of regexes (m=3).
+    m=3
+    for ((i = 1; i < n; i++)) do
+        for ((p = i+1; p < n; p++)) do
+            k=0
+            for ((j = 0; j < m; j++)) do
+                re[$j]=""
+                words[$j]=""
+                grep_result[$j]=0
+            done
+            for ((j = 0; j < n; j++)) do
+                if [ $j -eq $i ]; then
+                    k=$[k+1]
+                fi
+                if [ $j -eq $p ]; then
+                    k=$[k+1]
+                fi
+                re[$k]="${re[$k]}${regex[$j]}"
+            done
+            # echo "# i=$i p=$p re=${re[@]}"
+            all_found=1
+            for ((j = 0; j < m; j++)) do
+                wds=(`grep "^${re[$j]}$" "$HOME/dr/fun/itasoftware-puzzles/word.lst"`)
+                if [ ${#wds[@]} -eq 0 ]; then
+                    all_found=0
+                    break
+                fi
+                words[$j]="${wds[@]}"
+            done
+            if [ $all_found -eq 1 ]; then
+                # echo "# re=${re[@]}"
+                echo "(${words[0]}) (${words[1]}) (${words[2]}) "
+            fi
+        done   # p
+    done   # i
+fi            
