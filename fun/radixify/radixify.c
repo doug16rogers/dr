83d349f6f9804495f56fdb32aa2b6a44e487547e
Your Name <you@example.com>
2018-07-18 18:40:27 -0400

    Add support for reading binary. Add base64 and bytes/ASCII input.

--- fun/radixify/radixify.c
+++ fun/radixify/radixify.c
@@ -43,6 +43,14 @@ const char* g_input_digits = NULL;
 int g_input_radix = 0;
 
 /**
+ * The number of binary digits to read at a time. If 0 then read as text
+ * using fgets().
+ */
+#define INPUT_CHUNK_SIZE_DEFAULT 0
+#define INPUT_CHUNK_SIZE_MAX 0x4000
+size_t g_input_chunk_size = INPUT_CHUNK_SIZE_DEFAULT;
+
+/**
  * The list of output digits.
  */
 const char* g_output_digits = NULL;
@@ -62,24 +70,55 @@ int g_case_sensitive = 0;
  * Mapping for special digits lists.
  */
 typedef struct SpecialDigits_s {
-    char short_cut;
-    const char* digits;
-    const char* description;
+    char short_cut;             /**< Single-character short-cut for digit set. */
+    int radix;                  /**< 0 -> Use strlen(). */
+    const char* digits;         /**< List of characters to use as digits. */
+    const char* description;    /**< Description of this radix. */
 } SpecialDigits;
+
+/**
+ * ASCII 8-bit (just raw bytes).
+ */
+const char kAscii8[0x100] = {
+    0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
+    0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,
+    0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,0x29,0x2A,0x2B,0x2C,0x2D,0x2E,0x2F,
+    0x30,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x3A,0x3B,0x3C,0x3D,0x3E,0x3F,
+    0x40,0x41,0x42,0x43,0x44,0x45,0x46,0x47,0x48,0x49,0x4A,0x4B,0x4C,0x4D,0x4E,0x4F,
+    0x50,0x51,0x52,0x53,0x54,0x55,0x56,0x57,0x58,0x59,0x5A,0x5B,0x5C,0x5D,0x5E,0x5F,
+    0x60,0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x68,0x69,0x6A,0x6B,0x6C,0x6D,0x6E,0x6F,
+    0x70,0x71,0x72,0x73,0x74,0x75,0x76,0x77,0x78,0x79,0x7A,0x7B,0x7C,0x7D,0x7E,0x7F,
+    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8A,0x8B,0x8C,0x8D,0x8E,0x8F,
+    0x90,0x91,0x92,0x93,0x94,0x95,0x96,0x97,0x98,0x99,0x9A,0x9B,0x9C,0x9D,0x9E,0x9F,
+    0xA0,0xA1,0xA2,0xA3,0xA4,0xA5,0xA6,0xA7,0xA8,0xA9,0xAA,0xAB,0xAC,0xAD,0xAE,0xAF,
+    0xB0,0xB1,0xB2,0xB3,0xB4,0xB5,0xB6,0xB7,0xB8,0xB9,0xBA,0xBB,0xBC,0xBD,0xBE,0xBF,
+    0xC0,0xC1,0xC2,0xC3,0xC4,0xC5,0xC6,0xC7,0xC8,0xC9,0xCA,0xCB,0xCC,0xCD,0xCE,0xCF,
+    0xD0,0xD1,0xD2,0xD3,0xD4,0xD5,0xD6,0xD7,0xD8,0xD9,0xDA,0xDB,0xDC,0xDD,0xDE,0xDF,
+    0xE0,0xE1,0xE2,0xE3,0xE4,0xE5,0xE6,0xE7,0xE8,0xE9,0xEA,0xEB,0xEC,0xED,0xEE,0xEF,
+    0xF0,0xF1,0xF2,0xF3,0xF4,0xF5,0xF6,0xF7,0xF8,0xF9,0xFA,0xFB,0xFC,0xFD,0xFE,0xFF,
+};
 
 /**
  * List of supported special digits lists.
  */
 const SpecialDigits kSpecials[] = {
-    { 'u', "1",                "unary sequence of 1s" },
-    { 'b', "01",               "binary" },
-    { 'o', "01234567",         "octal" },
-    { 'd', "0123456789",       "decimal" },
-    { 'x', "0123456789abcdef", "hexadecimal lower case" },
-    { 'X', "0123456789ABCDEF", "hexadecimal upper case" },
-    { 'k', "!@#$%^&*()~`_-+={[}]|\\:;”’<,>.?/", "karnamian" },
+    { 'u', 0, "1",                "Unary sequence of 1s" },
+    { 'b', 0, "01",               "Binary" },
+    { 'o', 0, "01234567",         "Octal" },
+    { 'd', 0, "0123456789",       "Decimal" },
+    { 'x', 0, "0123456789abcdef", "Hexadecimal (lower)" },
+    { 'X', 0, "0123456789ABCDEF", "Hexadecimal (upper)" },
+    { 'k', 0, "!@#$%^&*()~`_-+={[}]|\\:;”’<,>.?/", "karnamian" },
     /* @todo(dr) Provide Unicode support. */
-    { 'F', "/E@A:SIU+DRJNFCKTZLWHYPQOBG\"MXV#", "Ferranti Mark 1 [MSB first, +=\xC2\xBD/#=\xC2\xA3]" },
+    { 'F', 0, "/E@A:SIU+DRJNFCKTZLWHYPQOBG\"MXV#", "Ferranti Mark 1 [MSB first, +=\xC2\xBD/#=\xC2\xA3]" },
+    { '6', 64, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+      "Base64 plain (AB...Zab..z01..9+/)" },
+    { 'U', 64, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+      "Base64 URL (AB...Zab..z01..9-_)" },
+    { '7', 128, kAscii8, "ASCII 7-bit (raw bytes)" },
+    { '8', 256, kAscii8, "ASCII 8-bit (raw bytes)" },
+    { 'g', 0, "TCAG", "Genetic DNA codons" },
+    { 'G', 0, "UCAG", "Genetic RNA codons" },
 };
 
 /**
@@ -133,7 +172,11 @@ void Usage(FILE* file, int exit_code) {
             , g_program);
     for (i = 0; i < kSpecialsCount; ++i) {
         const SpecialDigits* sd = &kSpecials[i];
-        fprintf(file, "        %c - %s (%s)\n", sd->short_cut, sd->description, sd->digits);
+        if (sd->radix > 0) {
+            fprintf(file, "        %c - %s\n", sd->short_cut, sd->description);
+        } else {
+            fprintf(file, "        %c - %s (%s)\n", sd->short_cut, sd->description, sd->digits);
+        }
     }
     fprintf(file,
             "\n"
@@ -144,7 +187,9 @@ void Usage(FILE* file, int exit_code) {
             "\n"
             "    -h:elp                      Show this usage information.\n");
     fprintf(file,
-            "\n"
+            "    -b:nary:-size=<num>         Number of bytes to read at a time (0=text) [%d].\n",
+            INPUT_CHUNK_SIZE_DEFAULT);
+    fprintf(file,
             "    -[no-]v:erbose              Print verbose (debug) messages. [%s-verbose]\n"
             , kDefaultVerbose ? "" : "-no");
     exit(exit_code);
@@ -206,14 +251,14 @@ void HandleOutputNumber(mpz_t number) {
         return;
     }
     if (1 == g_output_radix) {  /* Unary. */
-        while (mpz_cmp_ui(n, 0) > 0) {
+        while ((mpz_cmp_ui(n, 0) > 0) && !str_error(str)) {
             str_append_char(str, g_output_digits[0]);
             mpz_sub_ui(n, n, 1);
         }
     } else {
         mpz_t q;        /* quotient */
         mpz_init(q);
-        while (mpz_cmp_ui(n, 0) > 0) {
+        while ((mpz_cmp_ui(n, 0) > 0) && !str_error(str)) {
             int value = mpz_tdiv_q_ui(q, n, g_output_radix);    /* Returns remainder. */
             str_append_char(str, g_output_digits[value]);
             mpz_swap(q, n);
@@ -230,10 +275,14 @@ void HandleOutputNumber(mpz_t number) {
 /**
  * Process the output number.
  */
-void HandleInputString(const char* s) {
+void HandleInputString(const char* s, size_t len) {
     int in_number = 0;
+    size_t i;
     mpz_t n;
-    for (; *s; ++s) {
+    if (len == 0) {
+        len = strlen(s);
+    }
+    for (i = 0; i < len; ++s, ++i) {
         int value = g_input_digit_value[((uint8_t) *s)];
         if (value < 0) {        /* If not a valid digit... */
             if (in_number) {    /* If this is the end of a number... */
@@ -271,9 +320,23 @@ void HandleInputFromFile(FILE* file) {
     /*
      * TODO(dr) Handle unary separately using fgetch().
      */
-    char line[0x4000] = "";
-    while (fgets(line, sizeof(line), file)) {
-        HandleInputString(line);
+    char line[INPUT_CHUNK_SIZE_MAX + 1] = "";
+    size_t nelem = 0;
+    while (!feof(file)) {
+        line[0] = 0;
+        if (g_input_chunk_size > 0) {
+            nelem = fread(line, 1, g_input_chunk_size, file);
+            if (0 == nelem) {
+                break;
+            }
+        } else {
+            if (NULL == fgets(line, sizeof(line), file)) {
+                break;
+            }
+            nelem = strlen(line);
+        }
+        PrintVerbose("Read %d bytes from stdin.", (int) nelem);
+        HandleInputString(line, nelem);
     }
 }   /* HandleInputFromFile() */
 
@@ -298,7 +361,7 @@ void SetDigitsOrExit(InputOutputDigits which, const char* digits) {
             const SpecialDigits* sd = &kSpecials[i];
             if (*digits == sd->short_cut) {
                 digits = sd->digits;
-                radix = strlen(digits);
+                radix = (sd->radix == 0) ? strlen(digits) : sd->radix;
                 break;
             }
         }
@@ -311,9 +374,16 @@ void SetDigitsOrExit(InputOutputDigits which, const char* digits) {
                 if (digits[i] == digits[j]) {
                     PrintUsageError("Digit '%c' is repeated in %s digit set.", digits[i],
                                     (kInputDigits == which) ? "input" : "output");
-                } else if ((kInputDigits == which) &&
-                           !g_case_sensitive &&
-                           (tolower(digits[j]) == tolower(digits[i]))) {
+                }
+            }
+        }
+    }
+    if (kInputDigits == which) {
+        for (i = 1; i < radix; ++i) {
+            for (j = 0; j < i; ++j) {
+                if ((kInputDigits == which) &&
+                    !g_case_sensitive &&
+                    (tolower(digits[j]) == tolower(digits[i]))) {
                     PrintVerbose("Setting case sensitivity because %s digit [%d]='%c' and [%d]='%c'.",
                                  (kInputDigits == which) ? "input" : "output",
                                  j, digits[j], i, digits[i]);
@@ -321,8 +391,6 @@ void SetDigitsOrExit(InputOutputDigits which, const char* digits) {
                 }
             }
         }
-    }
-    if (kInputDigits == which) {
         g_input_digits = digits;
         g_input_radix = radix;
         for (i= 0; i < 0x100; ++i) {
@@ -474,10 +542,20 @@ int ParseOptions(int argc, char* argv[]) {
     int end_of_options = 0;
     for (i = 1; i < argc; ++i) {
         char* arg = argv[i];
+        const char* val = NULL;
         if (end_of_options || ('-' != *arg)) {
             non_option_argument_list[rval++] = arg;
         } else if (('-' == arg[1]) && (0 == arg[2])) {
             end_of_options = 1;
+        } else if (IsOption(arg, &val, "b:inary:-size")) {
+            int size = 0;
+            if (NULL == val) {
+                PrintUsageError("no binary chunk size provided in \"%s\"", arg);
+            } else if ((sscanf(val, "%d", &size) != 1) || (size < 0) || (size > INPUT_CHUNK_SIZE_MAX)) {
+                PrintUsageError("invalid binary chunk size provided in \"%s\"", arg);
+            }
+            g_input_chunk_size = size;
+            PrintVerbose("Set input chunk size to %d.", (int) g_input_chunk_size);
         } else if (IsOption(arg, NULL, "h:elp")) {
             Usage(stdout, 0);
         } else if (IsFlagOption(arg, &g_verbose, "v:erbose")) {
@@ -519,7 +597,7 @@ int main(int argc, char* argv[]) {
     } else {
         for (i = 3; i < argc; ++i) {
             PrintVerbose("Reading digits from command line argument \"%s\".", argv[i]);
-            HandleInputString(argv[i]);
+            HandleInputString(argv[i], 0);
         }
     }
     
