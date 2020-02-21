21bf9d3fb7585f0a93fa9140158ab2a34cf70aa7
Your Name <you@example.com>
2020-02-21 15:45:37 -0500

    Update site for extracting public IP address.

--- bin/wmip.sh
+++ bin/wmip.sh
@@ -2,6 +2,5 @@
 
 wget -q -O - \
      -U 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0' \
-     http://whatismyipaddress.com \
-| grep -i 'click for more about' \
-| sed -e 's/^.*lick for more about //' -e 's/".*$//'
+     https://www.whatismypublicip.com \
+    | egrep -o '[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+'
