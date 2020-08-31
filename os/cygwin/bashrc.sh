608bf7cbed75840a6eb9a013f5c8cfffa9ff00a0
Your Name <you@example.com>
2020-08-31 10:39:47 -0400

    Add virtualbox directory to path on Windows.

--- os/cygwin/bashrc.sh
+++ os/cygwin/bashrc.sh
@@ -1,6 +1,7 @@
 # Loaded by bashrc by .bashrc if ~/.dr/site contains cygwin.
 
 export PATH="$PATH:/cygdrive/c/tools:/cygdrive/c/tools/emacs/bin"
+export PATH="$PATH:/cygdrive/c/Program Files/Oracle/VirtualBox"
 
 eval `ssh-pageant.exe`
 
