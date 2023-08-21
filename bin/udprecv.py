633027ef92c6e1a47b040a5e7c4e75f1e3cfc312
Doug Rogers <doug.16.rogers@gmail.com>
2023-08-21 18:14:44 -0400

    Use arg parser for udpsend/udprecv.

--- bin/udprecv.py
+++ bin/udprecv.py
@@ -1,26 +1,86 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import argparse
+import logging
 import socket
 import sys
 
-udp_ip = "0.0.0.0"      # default is any
+DEFAULT_UDP_IP = "0.0.0.0"
+DEFAULT_COUNT = 0          # 0 means no limit.
 
-if len(sys.argv) < 2:
-    print("Usage: udprecv.py port [ip]")
-    exit(1)
+# Set up logger to write to stderr.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+# date_format = '%Y%m%d %H%M%S'
+# msec_format = '.%(msecs)03d'
+date_format = '%Y-%m-%d %H:%M:%S'
+msec_format = ''
+# message_time = f'%(asctime)s{msec_format} '
+message_time = ''
+message_level = '[%(levelname).1s] '
+formatter = logging.Formatter(fmt=f'{message_time}{message_level} %(message)s',
+                              datefmt=date_format)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-udp_port = int(sys.argv[1])
 
-if len(sys.argv) > 2:
-   udp_ip = sys.argv[2]
+def main(args):
+    """
+    Handle command line arguments and receive the UDP datagrams.
 
-print("Receiving on {}:{}".format(udp_ip, udp_port))
+    :param args: arguments parsed by `argparse`.
+    """
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    if args.quiet:
+        logger.setLevel(logging.ERROR)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((udp_ip, udp_port))
-while True:
-    message, addr = sock.recvfrom(1024)
-    if sys.version_info[0] > 2:
-        message = message.decode('utf-8')
-    print("Received:", message)
-    print("    From:", addr)
+    udp_ip = args.ip if args.ip else DEFAULT_UDP_IP
+    udp_port = args.port
+
+    logger.debug(f'ip: "{udp_ip}"')
+    logger.debug(f'port: "{udp_port}"')
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((udp_ip, udp_port))
+
+    duration_text = f' for {args.count} packet{"" if args.count==1 else "s"}'
+    if args.count == 0:
+        duration_text = ' indefinitely; use Ctrl+C to stop'
+
+    logger.debug(f'Listening on {udp_ip}:{udp_port}{duration_text}.')
+
+    try:
+        n = 0
+        while (args.count == 0) or (n < args.count):
+            n += 1
+            message, addr = sock.recvfrom(65516)
+            byte_count = len(message)
+            if not args.raw:
+                message = message.decode('utf-8')
+            logger.debug(f'Received {byte_count} bytes from {addr}.')
+            sys.stdout.write(message)
+    except KeyboardInterrupt:
+        logger.debug('Ctrl+C pressed. Exiting...')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Receive UDP data from a remote client.')
+    parser.add_argument('port', type=int, help='Local port to listen on.')
+    parser.add_argument('ip', nargs='?', help='Local IP address to bind to.'
+                        f' [{DEFAULT_UDP_IP}]',
+                        default=DEFAULT_UDP_IP)
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='Show debug information.')
+    parser.add_argument('--quiet', '-q', action='store_true',
+                        help='Show only errors.')
+    parser.add_argument('--hex', '-x', action='store_true',
+                        help='Convert content from binary to hexadecimal.')
+    parser.add_argument('--count', '-c', type=int, default=0,
+                        help='Number of datagrams to receive before exiting,'
+                        f' 0 for no limit. [{DEFAULT_COUNT}]')
+    parser.add_argument('--raw', '-b', action='store_true',
+                        help='Emit binary - do not convert output to UTF-8.')
+    args = parser.parse_args()
+    main(args)
--- bin/udpsend.py
+++ bin/udpsend.py
@@ -1,16 +1,106 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import argparse
+import logging
 import socket
+import string
 import sys
 
-if len(sys.argv) < 4:
-    print("Usage: udpsend.py ip port payload...")
-    exit(1)
+# Set up logger to write to stderr.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+# date_format = '%Y%m%d %H%M%S'
+# msec_format = '.%(msecs)03d'
+date_format = '%Y-%m-%d %H:%M:%S'
+msec_format = ''
+# message_time = f'%(asctime)s{msec_format} '
+message_time = ''
+message_level = '[%(levelname).1s] '
+formatter = logging.Formatter(fmt=f'{message_time}{message_level} %(message)s',
+                              datefmt=date_format)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-udp_ip = sys.argv[1]
-udp_port = int(sys.argv[2])
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((udp_ip, udp_port))
-for pi in range(3, len(sys.argv)):
-    sock.send(sys.argv[pi])
 
+def convert_from_hex(s):
+    """
+    Reads hexadecimal values from `s`, converting them into bytes.
+
+    Any non-hexadecimal values are ignored (but are considered a break between
+    hexadecimal pairs/singlets). For example, "4At4 n35" would be converted to
+    b"\x4a\x04\x35".
+
+    :param str s: string containing ASCII hexadecimal values (pairs)
+    """
+    data = b''
+    xdigs = ''
+    for c in s:
+        if c in string.hexdigits:
+            xdigs += c
+            if len(xdigs) == 2:
+                data += bytes.fromhex(xdigs)
+                xdigs = ''
+        else:
+            if len(xdigs) > 0:
+                data += bytes.fromhex('0' + xdigs)
+                xdigs = ''
+    if len(xdigs) > 0:
+        data += bytes.fromhex('0' + xdigs)
+    return data
+
+
+def main(args):
+    """
+    Handles command line arguments and send the UDP datagrams.
+
+    :param args: arguments parsed by `argparse`.
+    """
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    if args.quiet:
+        logger.setLevel(logging.ERROR)
+
+    data = None
+    if len(args.content) == 0:
+        logger.debug('Reading content from stdin...')
+        data = sys.stdin.buffer.read()
+        if args.hex:
+            data = convert_from_hex(data.decode('utf8'))
+    else:
+        content = ' '.join(args.content)
+        if args.hex:
+            data = convert_from_hex(content)
+        else:
+            data = content.encode('utf8')
+
+    udp_ip = args.ip
+    udp_port = args.port
+
+    logger.debug(f'ip: "{udp_ip}"')
+    logger.debug(f'port: "{udp_port}"')
+    logger.debug(f'data: {data}')
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect((udp_ip, udp_port))
+    logger.debug(f'Sending {len(data)} bytes to {udp_ip}:{udp_port}...')
+    sock.send(data)
+    sock.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Send UDP data to a remote IP address.')
+    parser.add_argument('ip', help='Remote host or IP address')
+    parser.add_argument('port', type=int, help='Remote port to send to')
+    parser.add_argument('content', nargs='*',
+                        help='Content (will be joined with spaces). '
+                             'If no content is provided it is read verbatim '
+                             'from stdin.')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='Show debug information.')
+    parser.add_argument('--quiet', '-q', action='store_true',
+                        help='Show only errors.')
+    parser.add_argument('--hex', '-x', action='store_true',
+                        help='Convert content from hexadecimal to binary.')
+    args = parser.parse_args()
+    main(args)
