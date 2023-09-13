be6120adbcaa2af66648f4ad1ac74854766bac69
Doug Rogers <doug.16.rogers@gmail.com>
2023-09-13 18:21:32 -0400

    Add git branch to prompt.

--- os/bash-prompt.sh
+++ os/bash-prompt.sh
@@ -10,6 +10,15 @@ color_host="$color_red"
 color_dir="$color_CYAN"
 color_hist="$color_reset"
 color_char="$color_reset"
+color_git="$color_magenta"
+
+git_branch_text=""
+
+# Download from https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+if [[ -e "$DR_OS_DIR/git-prompt.sh" ]]; then
+    source "$DR_OS_DIR/git-prompt.sh"
+    git_branch_text=" \$(__git_ps1 '$color_git(%s)$color_reset')"
+fi
 
 prompt_char="$"
 
@@ -43,6 +52,6 @@ prompt_hist="\`history 1 | awk '{print \$1}' | (read _hist && echo \$[_hist+1])\
 # Readline (on MacOS anyway) struggles with complicated prompts, especially
 # when there are escape sequences embedded in the prompt after the last
 # newline. So just have the prompt character + space after the last newline.
-export PS1="\n$color_hist$prompt_hist $color_time${prompt_date} $color_user\u$color_reset@$color_host\h$color_reset $color_dir\w$color_reset\n$prompt_char "
+export PS1="\n$color_hist$prompt_hist $color_time${prompt_date} $color_user\u$color_reset@$color_host\h$color_reset$git_branch_text $color_dir\w$color_reset\n$prompt_char "
 
 # echo "PS1=\"$PS1\""
